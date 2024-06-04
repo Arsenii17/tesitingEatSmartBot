f2026fcd80880986113afd91271c7a196087511d
Arsenii17 <107906084+Arsenii17@users.noreply.github.com>
2024-06-04 11:01:36 +0600

    Update Php_Query.php

--- Php_Query.php
+++ Php_Query.php
@@ -1,15 +1,17 @@
+
+
+
+
 <?php
-// Функция, которая возвращает данные
-function getData($user_id) {
+    $user_id = $_POST['argument'];
+    // ваш код для обработки аргумента
     // Здесь ваш код для получения данных
-
-
     $connect_data = "host=rc1b-2tdd3qyqwzpike23.mdb.yandexcloud.net port=6432 dbname=eat_smart user=veron password=codemates";
     $db_connect = pg_connect($connect_data);
     if (!$db_connect) {
         die("Ошибка подключения: " . pg_result_error());
     }
-
+    echo 'ЭТО ЮЗЕР АЙДИ ПЕРЕДАННЫЙ В ЗАПРОСЕ:' . $user_id . "КОНЕц";
     $query = pg_query($db_connect, "SELECT * FROM user_history");
 
     if (!$query) {
@@ -21,13 +23,17 @@ function getData($user_id) {
         echo "<p> User_id: {$result['user_id']}. Съедено или нет: {$result['is_eat']}  Название: {$result['food_name']} Время: {$result['time']} </p> ";
     }
 
+    $data = [
+        "food_name" => $result['food_name'],
+        "is_eat" =>  $result['is_eat'],
+    ];
 
     pg_free_result($query);
     pg_close();
 
     return json_encode($data);
-}
 
-// Возвращаем данные в формате JSON
-echo getData($user_id);
+
+    $result = 'результат обработки аргумента';
+    echo $result;
 ?>
